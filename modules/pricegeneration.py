74cd2d5821fc1be961c56624d505d5df614f64f0
experiencedft <14246657+experiencedft@users.noreply.github.com>
2021-02-03 19:08:33 +0100

    Add bounded random walk function

--- modules/pricegeneration.py
+++ modules/pricegeneration.py
@@ -1,6 +1,6 @@
 ''' 
 
-The Price Generation module is a toolkit to generate price actions which are simplistic, time independent, local extrema based profiles.  
+The Price Generation module is a toolkit to generate price various price action profiles from simple linear interpolation to random walks with prescribed variance.  
 
 '''
 
@@ -9,7 +9,7 @@ import numpy as np
 
 def createUptrend(init_price, final_price, n_corrections,  amplitude_list):
     '''
-    Returns a numpy array representing an uptrend. Here,  uptrend is defined as a final price higher than the initial price, with the possibility of a user specified number of corrections with user specified amplitudes along the way. 
+    Returns a numpy array representing an uptrend. Here,  uptrend is defined as a final price higher than the initial price, with the possibility of a user specified number of corrections with user specified amplitudes along the way. A simple interpolation between local maxima is used. 1000 sized array with no reference to time. 
 
     Parameters:
         init_price (float): Initial price.
@@ -57,6 +57,7 @@ def createUptrend(init_price, final_price, n_corrections,  amplitude_list):
 def createDowntrend(init_price, final_price, n_bounces, amplitude_list): 
     '''
     Returns a numpy array representing a downtrend. Here, downtrend is defined as a final price lower than the initial price, with the possibility of a user specified number of bounces with user specified amplitudes along the way. 
+    A simple interpolation between local maxima is used. 1000 sized array with no reference to time.  
 
     Parameters:
         init_price (float): Initial price.
@@ -103,7 +104,7 @@ def createDowntrend(init_price, final_price, n_bounces, amplitude_list):
     
 def createSideways(average_price, amplitude, n_cycles):
     ''' 
-    Returns a numpy array representing a sideways price action. Here, sideways is defined as the going up and down above an average price, starting at that average and coming back to it in one or more cycles with an amplite define in percentage of the average.
+    Returns a numpy array representing a sideways price action. Here, sideways is defined as the going up and down above an average price, starting at that average and coming back to it in one or more cycles with an amplite define in percentage of the average. Simple sine wave, no reference to time.
 
     Parameters:
         average_price (float): Average price, which is also the initial price here.
@@ -115,3 +116,50 @@ def createSideways(average_price, amplitude, n_cycles):
     priceArray = average_price*(1+amplitude*np.sin(2*n_cycles*np.pi*x))
 
     return priceArray
+
+def boundedRandomWalk(length, lower_bound,  upper_bound, start, end, std):
+    '''
+
+    Taken from user igrinis on Stack Overflow: https://stackoverflow.com/a/47005958/5433929
+
+    From the post:
+
+    You can see it as a solution of geometric problem. 
+    
+    The trend_line is connecting your start and end points, and have margins defined by lower_bound and upper_bound. 
+    
+    rand is your random walk, rand_trend it's trend line and rand_deltas is it's deviation from the rand trend line. 
+    
+    We collocate the trend lines, and want to make sure that deltas don't exceed margins. When rand_deltas exceeds the allowed margin, we "fold" the excess back to the bounds.
+
+    At the end you add the resulting random deltas to the start=>end trend line, thus receiving the desired bounded random walk.
+
+    The std parameter corresponds to the amount of variance of the random walk.
+
+    In this version "std" is not promised to be the "interval". 
+
+
+    '''
+    assert (lower_bound <= start and lower_bound <= end)
+    assert (start <= upper_bound and end <= upper_bound)
+
+    bounds = upper_bound - lower_bound
+
+    rand = (std * (np.random.random_sample(length) - 0.5)).cumsum()
+    rand_trend = np.linspace(rand[0], rand[-1], length)
+    rand_deltas = (rand - rand_trend)
+    rand_deltas /= np.max([1, (rand_deltas.max()-rand_deltas.min())/bounds])
+
+    trend_line = np.linspace(start, end, length)
+    upper_bound_delta = upper_bound - trend_line
+    lower_bound_delta = lower_bound - trend_line
+
+    upper_slips_mask = (rand_deltas-upper_bound_delta) >= 0
+    upper_deltas =  rand_deltas - upper_bound_delta
+    rand_deltas[upper_slips_mask] = (upper_bound_delta - upper_deltas)[upper_slips_mask]
+
+    lower_slips_mask = (lower_bound_delta-rand_deltas) >= 0
+    lower_deltas =  lower_bound_delta - rand_deltas
+    rand_deltas[lower_slips_mask] = (lower_bound_delta + lower_deltas)[lower_slips_mask]
+
+    return trend_line + rand_deltas
